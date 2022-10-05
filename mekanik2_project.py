a8e338f04e4a686fc93ade0f20333863777ee650
GustafIvarsson <114744967+GustafIvarsson@users.noreply.github.com>
2022-10-05 09:15:44 +0200

    Add files via upload
    morgon 5/10 09:15

--- mekanik2_project.py
+++ mekanik2_project.py
@@ -25,7 +25,7 @@ L = 1
 a = 0.25
 I_skiva = M*L**2 /12
 I_vagn = 0.01
-F =0
+F =70
 g = 9.82
 
 # x= (sin(y[1])/cos(y[1]))*((-(F*a -M*g*(l/2 -a)*cos(y[1]))/y[0])/m +2*y[2]*y[3] +y[0]*((F*a-M*g*(l/2-a)*cos(y[1])-m*g*y[0]*cos(y[1]))/(i_skiva + M*(l/2-a)**2 +I_vagn +m*(y[0])**2)) +y[2]*(y[3])**2)
@@ -40,59 +40,55 @@ ypos = []
 
 y0 = [0.3, -pi/32, 0 , 0]
 
-tend = 20
+tend = 10
 frames = 4000
-te= 4000
+te= 350
 
 def animate_events(i,xpos,ypos):
     redDot.set_data(xpos[i],ypos[i])
     return redDot,
 
-# change the s(y) function depending on the different cases
-
-    #case 1,a) (S0*(y/R)**1)
-    #case 1,b) (S0*(y/R)**(-1))
-    #case 1,c) (S0*(y/R)**(-1/10))
-    #case 2) (S0*((y-R)/R))
-    #case 3) (S0*math.exp((y-R)/R)*cos(y/R))
-
-def s(y):
-    return (S0*(y/R)**1)
-
-
 
 def f(t, y):
-    if(abs(cos(y[1])) < 0.001 or abs(y[0]) < 0.001):
-        return[y[2],
-               y[3],
-               (sin(y[1]+0.001)/cos(y[1]+0.001))*((-(F*a -M*g*(L/2 -a)*cos(y[1]))/(y[0]+0.01))/m +2*y[2]*y[3] +y[0]*((F*a-M*g*(L/2-a)*cos(y[1])-m*g*y[0]*cos(y[1]))/(I_skiva + M*(L/2-a)**2 +I_vagn +m*(y[0])**2)) +y[2]*(y[3])**2),
-               (F*a -M*g*(L/2-a)*cos(y[1]) -m*g*y[0]*cos(y[1]))/(I_skiva + M*(L/2-a)**2 +I_vagn +m*(y[0])**2)]
+    if abs(y[0])< 0.001:
+        return  [y[2],
+                y[3],
+                (sin(y[1])/cos(y[1]))*((-(F*a -M*g*(L/2 -a)*cos(y[1]))/(y[0]+0.002))/m +2*y[2]*y[3]+y[0]*((F*a-M*g*(L/2-a)*cos(y[1])-m*g*y[0]*cos(y[1]))/
+                                          (I_skiva + M*(L/2-a)**2 +I_vagn +m*(y[0])**2)) +y[2]*(y[3])**2),
+                (F*a -M*g*(L/2-a)*cos(y[1]) -m*g*y[0]*cos(y[1]))/(I_skiva + M*(L/2-a)**2 +I_vagn +m*(y[0])**2)]
+    
+    elif abs(cos(y[1]))< 0.001:
+        return  [0,0,0,0]
+    
     else:
         return [y[2],
                 y[3],
-                (sin(y[1])/cos(y[1]))*((-(F*a -M*g*(L/2 -a)*cos(y[1]))/y[0])/m +2*y[2]*y[3] +y[0]*((F*a-M*g*(L/2-a)*cos(y[1])-m*g*y[0]*cos(y[1]))/(I_skiva + M*(L/2-a)**2 +I_vagn +m*(y[0])**2)) +y[2]*(y[3])**2),
+                (sin(y[1])/cos(y[1]))*((-(F*a -M*g*(L/2 -a)*cos(y[1]))/y[0])/m +2*y[2]*y[3] 
+                                       +y[0]*((F*a-M*g*(L/2-a)*cos(y[1])-m*g*y[0]*cos(y[1]))/
+                                              (I_skiva + M*(L/2-a)**2 +I_vagn +m*(y[0])**2)) +y[2]*(y[3])**2),
                 (F*a -M*g*(L/2-a)*cos(y[1]) -m*g*y[0]*cos(y[1]))/(I_skiva + M*(L/2-a)**2 +I_vagn +m*(y[0])**2)]
 
 
-sol = solve_ivp(f, [0, 20], y0, method='Radau', t_eval=np.linspace(0, 20, frames))
-
+sol = solve_ivp(f, [0, tend], y0, method='Radau', t_eval=np.linspace(0, tend, frames))
+#i=0
 
 for i in range(te): #frames
+#while (abs(sol.y[0][i])< L):
 
-    if sol.y[0][i]!= 0 and sol.y[1][i]!= 0:
-        radius = sol.y[0][i]
-        angle = sol.y[1][i]
+    radius = sol.y[0][i]
+    angle = sol.y[1][i]
 
-        #print(len(sol.y[0]))
+    #print(len(sol.y[0]))
 
-        y = radius* sin(angle)
+    y = radius* sin(angle)
 
-        x = radius*cos(angle)
-        xpos.append(x)
-        ypos.append(y)
-    else:
-        print("hej")
-        pass
+    x = radius*cos(angle)
+    xpos.append(x)
+    ypos.append(y)
+
+    
+    print(sol.y[0][i])
+    #i=i+1
 
 
 
@@ -102,7 +98,7 @@ plt.xlabel('x')
 plt.ylabel('y')
 
 redDot, = plt.plot(xpos[0], ypos[0], 'ro')
-anim = animation.FuncAnimation(fig, animate_events, frames=te, fargs=(xpos, ypos),
+anim = animation.FuncAnimation(fig, animate_events, frames=i, fargs=(xpos, ypos),
                                interval=10, blit=True, repeat= True)
 
 
